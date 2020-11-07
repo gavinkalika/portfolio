afdce6e21c49135377694c3a9fd9fa7e2f39d041
gavin <4485067+gavinkalika@users.noreply.github.com>
2020-11-07 13:46:37 -0500

    wip

---
+++ app/static/js/downloadResume.js
@@ -0,0 +1,12 @@
+(function($) {
+
+//jQuery('#download-resume').click(function() {
+//
+//      ga('send', {
+//      eventCategory: 'Interactive',
+//      eventAction: 'click',
+//      eventLabel: 'Download resume'
+//    });
+//});
+
+})(jQuery);
